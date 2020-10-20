192db44eb347453805debb7e1e87d8558ecb718a
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2020-10-20 12:50:12 -0600

    Redundant bases removed

--- reference/rolecheck.py
+++ reference/rolecheck.py
@@ -1,6 +1,3 @@
-base = "**1**: Custodians - 150 EXP \n **2**: Record Keeper - 350 EXP \n **3**: Book Keeper - 750 EXP \n **4**: " \
-       "Librarian - 1500 EXP \n **5**: Vizier - 2400 EXP \n **6**: Grand Vizier - 4770 EXP "
-
 one = "**1**: Custodians - 150 EXP"
 two = "**2**: Record Keeper - 350 EXP"
 three = "**3**: Book Keeper - 750 EXP"
@@ -10,7 +7,6 @@ six = "**6**: Grand Vizier - 4770 EXP"
 rhd = "*(You already have that role)*"
 ino = "*(You must buy roles in order)*"
 
-base2 = one + "\n" + two + "\n" + three + "\n" + four + "\n" + five + "\n" + six
 base3 = f"{one} \n {two} \n {three} \n {four} \n {five} \n {six}"
 
 c = f"~~{one}~~ {rhd} \n {two} \n {three} \n {four} \n {five} \n {six}"
