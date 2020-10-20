2eb3a3a275f0443a701d2c041f10f4c209051c27
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2020-10-20 12:49:27 -0600

    role already had checks added

--- reference/rolecheck.py
+++ reference/rolecheck.py
@@ -1,2 +1,21 @@
 base = "**1**: Custodians - 150 EXP \n **2**: Record Keeper - 350 EXP \n **3**: Book Keeper - 750 EXP \n **4**: " \
        "Librarian - 1500 EXP \n **5**: Vizier - 2400 EXP \n **6**: Grand Vizier - 4770 EXP "
+
+one = "**1**: Custodians - 150 EXP"
+two = "**2**: Record Keeper - 350 EXP"
+three = "**3**: Book Keeper - 750 EXP"
+four = "**4**: Librarian - 1500 EXP"
+five = "**5**: Vizier - 2400 EXP"
+six = "**6**: Grand Vizier - 4770 EXP"
+rhd = "*(You already have that role)*"
+ino = "*(You must buy roles in order)*"
+
+base2 = one + "\n" + two + "\n" + three + "\n" + four + "\n" + five + "\n" + six
+base3 = f"{one} \n {two} \n {three} \n {four} \n {five} \n {six}"
+
+c = f"~~{one}~~ {rhd} \n {two} \n {three} \n {four} \n {five} \n {six}"
+rk = f"~~{one}~~ {rhd} \n ~~{two}~~ {rhd} \n {three} \n {four} \n {five} \n {six}"
+bk = f"~~{one}~~ {rhd} \n ~~{two}~~ {rhd} \n ~~{three}~~ {rhd} \n {four} \n {five} \n {six}"
+li = f"~~{one}~~ {rhd} \n ~~{two}~~ {rhd} \n ~~{three}~~ {rhd} \n ~~{four}~~ {rhd} \n {five} \n {six}"
+v = f"~~{one}~~ {rhd} \n ~~{two}~~ {rhd} \n ~~{three}~~ {rhd} \n ~~{four}~~ {rhd} \n ~~{five}~~ {rhd} \n {six}"
+gv = f"~~{one}~~ {rhd} \n ~~{two}~~ {rhd} \n ~~{three}~~ {rhd} \n ~~{four}~~ {rhd} \n ~~{five}~~ {rhd} \n ~~{six}~~ {rhd}"
