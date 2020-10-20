44c00c570d0f7fca48015e7494c643f6e7c91896
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2020-10-19 20:35:09 -0600

    Role work

--- cogs/roles.py
+++ cogs/roles.py
@@ -2,6 +2,16 @@ import discord
 from discord.ext import commands
 
 
+roleids =	{
+    "1": 767921468272934972,
+    "2": 767921943184932904,
+    "3": 767922049443954719,
+    "4": 767921702314573834,
+    "5": 767922513664802827,
+    "6": 767921509867454504
+}
+
+
 class Roles(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
@@ -11,14 +21,21 @@ class Roles(commands.Cog):
         aliases=["t"]
     )
     # @commands.dm_only
-    async def buy_role(self, ctx:commands.context):
-        embed = discord.Embed(title="MegaBot Store", color=0x9AD2C9)
-        embed.add_field(name="** **", value="**1**: Custodians - 150 EXP \n **2**: Record Keeper - 350 EXP \n **3**: "
-                                            "Book Keeper - 750 EXP \n **4**: Librarian - 1500 EXP \n **5**: Vizier - "
-                                            "2400 EXP \n **6**: Grand Vizier - 4770 EXP" , inline=False)
-        embed.add_field(name="** **", value="*Use* `!buy roles role-number` *to buy a role*")
-        embed.set_footer(text="MegaBot Remake v1.0.0")
-        await ctx.send(embed=embed)
+    async def buy_role(self, ctx: commands.Context, rnumber=None):
+        if rnumber:
+
+        else:
+            try:
+                embed = discord.Embed(title="MegaBot Store", color=0x9AD2C9)
+                embed.add_field(name="** **", value="**1**: Custodians - 150 EXP \n **2**: Record Keeper - 350 EXP \n "
+                                                    "**3**: Book Keeper - 750 EXP \n **4**: Librarian - 1500 EXP \n "
+                                                    "**5**: Vizier - 2400 EXP \n **6**: Grand Vizier - 4770 EXP",
+                                inline=False)
+                embed.add_field(name="** **", value="*Use* `!buy roles role-number` *to buy a role*")
+                embed.set_footer(text="MegaBot Remake v1.0.0")
+                await ctx.send(embed=embed)
+            except discord.HTTPException as err:
+                await ctx.send(f"Error: {err.text}")
 
 
 def setup(client: commands.Bot):
