5601bd0fc16cb08721c289a5a5b1a97208e80aad
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2020-10-21 18:30:50 -0600

    Changed my mind again. Back to feedback.py

--- cogs/feedback.py
+++ cogs/feedback.py
@@ -6,6 +6,10 @@ class Feedback(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
 
+    @commands.command(
+        name="suggest"
+    )
+
 
 def setup(client: commands.Bot):
     client.add_cog(Feedback(client))
